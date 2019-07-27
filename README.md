22c5d487fcc851a899d8833d24d36ca5de039b99
Tyler Kluszczynski <tylerkluszczynski@gmail.com>
2019-07-27 14:26:20 -0700

    Switched advisory file to a temporary file.
    Also made switching between linux and windows versions easier.

--- README.md
+++ README.md
@@ -12,13 +12,10 @@ This is set up to work with the following MetroLink lines:
 
 Currently, this is one of the only ways to get email updates from Metrolink about specific train advisories (which is why I created it.)
 
-In order to get this working, navigate to the metrolink_advisories.py script and follow the instructions at the top. You will need to fill in 3 variables, along with your train numbers.
+In order to get this working, navigate to the metrolink_advisories.py script and follow the instructions at the top. You will need to fill in a handful of variables along with your train numbers.
 
+It is recommended to run this on a Linux server.
 
-It is recommended to run this script on a linux server but if you want to set it up on a local machine, you can use the [Windows Task Scheduler](https://stackoverflow.com/questions/4249542/run-a-task-every-x-minutes-with-windows-task-scheduler) (keep in mind, your machine must be on and connected to the internet in order for you to receive email updates).
-
-If you are running a linux server, just run this script with a [cronjob](https://askubuntu.com/questions/799023/how-to-set-up-a-cron-job-to-run-every-10-minutes) every 5 minutes or so.
-
-Please note that the requests_html library used in this script doesn't support ARM devices (such as raspberry pi).
+Please note that the requests_html library used in this script doesn't support ARM devices (such as raspberry pi), and is complex to set up with a Docker container.
 
 Note that this script is not endorsed, supported by, or affiliated with Metrolink.
--- metrolink_advisories.py
+++ metrolink_advisories.py
@@ -1,42 +1,42 @@
 from requests_html import HTMLSession
-from time import strftime
 from email.mime.text import MIMEText
-import datetime, smtplib, ssl, hashlib, os
+import datetime, smtplib, ssl, hashlib, os, tempfile, time
 
 """
-Requires - requests_html (pip install requests_html).
+First, install these requirements.
+  - pyhton3-pip3
+  - python3-lxml
+  - requests       (via pip3 install requests)
+  - requests-html  (via pip3 install requests_html)
 
-SENDER_EMAIL - email address that will send the email. Requres a Google App Key.
+Then, modify the below variables.
+  - SENDER_EMAIL   - email address that will send the email.
+  - GOOGLE_APP_KEY - google app key for the above google account.
+      - https://support.google.com/accounts/answer/185833
+      - Should be 16 characters, don't enter spaces.
+  - TO_EMAIL       - email address that will recieve the email.
+      - It is reccomended to use a different email address to ensure you get the notification.
+  - FILENAME       - name of the temporary file to generate. Don't need to change this normally.
+  - WINDOWS        - set to "False" if on a linux machine or docker container.
+  - WEEKEND_NOTIFY - set to "True" if you want to be notified of train delays on weekends.
 
-GOOGLE_APP_KEY -
-Go to your Google Account, click "Security",
-then click "App passwords" in the "Signing in to Google" section, then generate
-an app password for the Mail app.
-https://support.google.com/accounts/answer/185833
+Next, put your train numbers in retrieve_advisories() method where it says "//Modify array with your train numbers".
 
-Use this 16 character password as the "GOOGLE_APP_KEY" variable below.
+Finally, go to the very bottom.
+  - Uncomment "main("","")" if you want this script to run once.
+  - or uncomment "runon_schedule(300.00)" to keep this script running, and check every 300 seconds.
+    - You can also change the number of seconds (e.g. 500.0)
+    - Do not do this if you are using GCP cloud functions or AWS Lambda functions to run this.
+    - Add this python script to your startup.
 
-TO_EMAIL - email address to send the email to. It is recommended to use a different
-email address, as it will guarantee you will be notified whenever you recieve an email alert.
-(On some Android versions of Gmail, you won't be notified about an email you send to yourself).
-
-FILENAME - name of the file to store past advisories in.
-This file should be cleaned out semi-regularly, but this is not required.
-
-WEEKEND_NOTIFY - set to True if you want to be emailed about advisories on weekends.
-
-Also note that if you are on a linux OS, change:
-    today = strftime("%#m/%#d/%Y") to
-    today = strftime("%-m/%-d/%Y")
-in the handle_advisories() method.
-
-Set your train numbers in the retrieve_advisories method where it says //Modify array with your train numbers
+Remember to add execution permissions to this script on linux.
 """
 
-SENDER_EMAIL = ""
+SENDER_EMAIL   = ""
 GOOGLE_APP_KEY = ""
-TO_EMAIL = ""
-FILENAME = "sent_advisories.txt"
+TO_EMAIL       = ""
+FILENAME       = "sent_advisories.txt"
+WINDOWS        = True
 WEEKEND_NOTIFY = False
 
 class ServiceAdvisory:
@@ -136,7 +136,7 @@ def update_advisory_file(filename, advisory):
     advisory - ServiceAdvisory object.
     """
 
-    advisory_file = open(filename, "a+")
+    advisory_file = open(tempfile.gettempdir() + "/" + filename, "a+")
     advisory_file.write(advisory.response + "\n")
     advisory_file.close()
 
@@ -151,20 +151,22 @@ def been_notified(filename, advisory):
     Boolean, True if the user has already been notified (if advisory's date and time appears in the file).
     """
 
-    if not os.path.exists(filename):
-        open(filename,"a+").close()
+    if not os.path.exists(tempfile.gettempdir() + "/" + filename):
+        open(tempfile.gettempdir() + "/" + filename,"a+").close()
 
     advisory_response = advisory.response
-    advisory_file = open(filename, "r")
+    advisory_file = open(tempfile.gettempdir()+ "/" + filename, "r")
 
     for line in advisory_file:
         if line.strip() == advisory_response.strip():
             print("User has already been notified of this advisory.")
+            advisory_file.close()
             return True
 
+    advisory_file.close()
     return False
 
-def handle_advisories(response_list, reciever_email, sender_email, filename, password, weekend_notify):
+def handle_advisories(response_list, reciever_email, sender_email, filename, password, weekend_notify, windows):
     """ Handles advisories, acts as a main method.
     Will not notify a user about advisories on weekends by default.
 
@@ -176,8 +178,11 @@ def handle_advisories(response_list, reciever_email, sender_email, filename, pas
     password        - string, Google App Key with Mail permissions.
     weekend_notify  - boolean, true to notify on weekends.
     """
-    # Change # to - if on linux, in the following variable:
-    today = strftime("%#m/%#d/%Y")
+    # Adding linux compatability for the datetime string.
+    if(windows):
+        today = time.strftime("%#m/%#d/%Y")
+    else:
+        today = time.strftime("%-m/%-d/%Y")
 
     # Looping through each advisory for your trains.
     for metro_response in response_list:
@@ -189,10 +194,25 @@ def handle_advisories(response_list, reciever_email, sender_email, filename, pas
                 if(not been_notified(filename, ServiceAdvisory(metro_response))):
                     send_mail(ServiceAdvisory(metro_response), reciever_email, sender_email, password)
                     update_advisory_file(filename, ServiceAdvisory(metro_response))
+    print("Finished.")
 
 # Arguments and method included if you want to attempt to get this working with AWS Lambda.
-def main(event,context):
+def main(context, response):
+    print("Starting execution.")
     response_list = retrieve_advisories()
-    handle_advisories(response_list, TO_EMAIL, SENDER_EMAIL, FILENAME, GOOGLE_APP_KEY, WEEKEND_NOTIFY)
+    handle_advisories(response_list, TO_EMAIL, SENDER_EMAIL, FILENAME, GOOGLE_APP_KEY, WEEKEND_NOTIFY, WINDOWS)
 
-main("","")
+# Running continuously to avoid cron (for docker and linux).
+def runon_schedule(delay_seconds):
+    starttime = time.time()
+    while True:
+        main("","")
+        print("Waiting " +str(delay_seconds)+ " seconds.")
+        time.sleep(delay_seconds - ((time.time() - starttime) % delay_seconds))
+        print("Running again.\n")
+
+# To just run once:
+# main("", "")
+
+# To run on schedule:
+# runon_schedule(300.00)
